ba03dffe77b7826ba89d43dde5576f7c39807441
sheep@ghostwheel <sheep@ghostwheel>
2009-08-03 00:55:56 +0200

    mention bsd license

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ config = dict(
     data_files=[
         ('share/doc/creole.py/examples', ['creole2html.py']),
         ('share/doc/creole.py/tests', ['test.py']),
+        ('share/doc/creole.py/', ['COPYING']),
     ],
     platforms='any',
     classifiers=[
